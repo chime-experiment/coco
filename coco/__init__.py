4b0efb79ade2e20b5f096ff96d5e8de049299d73
Rick Nitsche <rick@phas.ubc.ca>
2019-04-12 10:46:29 -0700

    add setup.py and versioneering

--- coco/__init__.py
+++ coco/__init__.py
@@ -1,3 +1,7 @@
 from .endpoint import Endpoint
 from .slack import SlackExporter
 from .master import Master
+
+from ._version import get_versions
+__version__ = get_versions()['version']
+del get_versions

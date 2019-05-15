0c6835e10914e999f8b0a041a29ddc2fbe50eff8
Rick Nitsche <rick@phas.ubc.ca>
2019-05-15 16:51:51 -0700

    feat(master): register start and config with comet

--- coco/__init__.py
+++ coco/__init__.py
@@ -1,8 +1,7 @@
 """coco: A Config Controller."""
-from .endpoint import Endpoint
-from .slack import SlackExporter
-from .master import Master
-
 from ._version import get_versions
 __version__ = get_versions()['version']
 del get_versions
+from .endpoint import Endpoint
+from .slack import SlackExporter
+from .master import Master

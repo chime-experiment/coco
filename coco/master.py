366a62c33f6b1cfd5b93644a8a39fdbf3b298b64
Rick Nitsche <rick@phas.ubc.ca>
2019-06-06 12:09:14 -0700

    feat: implement task queue using sanic and redis
    Sanic runs with a configurable amount of processes to receive all
    endpoint calls. They are passed in a FIFO queue using redis and the
    endpoint waits until the task is marked as done and forwards the result
    as the HTTP reply.
    
    A seperate worker process runs the tasks one by one taking them from the
    queue from redis and hands back the result. If the endpoint exists is
    checked here.

--- coco/master.py
+++ coco/master.py
@@ -5,14 +5,57 @@ This is the core of coco. Endpoints are loaded and called through the master mod
 Also loads the config.
 """
 import datetime
+import orjson as json
 import os
+import time
 import yaml
 
+from multiprocessing import Process
+from sanic import Sanic
+from sanic.log import access_logger as logger
+from sanic.response import text
+from sanic_redis import SanicRedis
 from comet import Manager, CometError
 
-from . import Endpoint
-from . import SlackExporter
-from . import __version__
+from . import Endpoint, SlackExporter, worker, __version__
+
+app = Sanic(__name__)
+app.config.update({"REDIS": {"address": ("127.0.0.1", 6379)}})
+redis = SanicRedis(app)
+
+
+@app.route("/<endpoint>", methods=["GET", "POST"])
+async def master_endpoint(request, endpoint):
+    """
+    Receive all HTTP calls.
+
+    Master endpoint. Passes all endpoint calls on to redis and blocks until completion.
+    """
+    # create a unique name for this task: <process ID>-<POSIX timestamp>
+    name = f"{os.getpid()}-{time.time()}"
+    logger.debug(f"name: {name}")
+
+    with await redis.conn as r:
+
+        # Give the task to redis
+        await r.hmset(
+            name,
+            "method",
+            request.method,
+            "endpoint",
+            endpoint,
+            "request",
+            json.dumps(request.json),
+        )
+
+        # Add task name to queue
+        await r.rpush("queue", name)
+
+        # Wait for the result
+        logger.debug(f"Waiting for: {name}:res")
+        result = (await r.blpop(f"{name}:res"))[1]
+        await r.delete(f"{name}:res")
+    return text(int(result))
 
 
 class Master:
@@ -27,7 +70,25 @@ class Master:
         self.slacker = SlackExporter(self.slack_url)
         config["endpoints"] = self._load_endpoints()
         self._register_config(config)
+        self.queue = Process(target=worker.main_loop, args=(self.endpoints,))
+        self.queue.start()
+        self._start_server()
 
+    def __del__(self):
+        """
+        Destruct :class:`Master`.
+
+        Join the worker thread.
+        """
+        self.queue.join()
+
+    def _start_server(self):
+        """Start a sanic server."""
+        app.run(
+            host="0.0.0.0", port=self.port, workers=self.n_workers, debug=True, access_log=True
+        )
+
+    # TODO: remove this? endpoints should only be called by worker process
     def call_endpoint(self, name):
         """
         Call an endpoint.
@@ -85,6 +146,8 @@ class Master:
         except KeyError:
             self.slack_url = None
             print("Key 'slack_webhook' not found. Slack messaging DISABLED.")
+        self.port = config["port"]
+        self.n_workers = config["n_workers"]
         return config
 
     def _load_endpoints(self):

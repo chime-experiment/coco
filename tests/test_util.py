aceb241d279911fc9935e30ca0cfacc8b9bf071b
D. V. Wiebe <dvw@phas.ubc.ca>
2026-05-15 12:26:55 -0700

    test: Recover the old PersistentState test
    It's probably duplicating some of the testing I rewrote, but
    it can't hurt to have it, too.

--- tests/test_util.py
+++ tests/test_util.py
@@ -230,3 +230,100 @@ def test_hash_dict():
     b = {"j": 1, "b": [{"c": 2, "d": 3}, {"e": 4, "f": 5}], "g": {"h": 6, "i": 7}}
 
     assert util.hash_dict(a) != util.hash_dict(b)
+
+
+def test_hash_dict_value():
+    """Check the values returned by hash_dict.
+
+    The hash dict algorithm as three steps:
+    * sort the keys of the input dict (see util.sort_dict)
+    * serialize the data using MessagePack
+
+        see https://github.com/msgpack/msgpack/blob/master/spec.md
+
+    * Compute the MD5 hash of the serialized data.
+    """
+
+    # Here's our test dict
+    a = {"g": {"i": 7, "h": 6}, "b": [{"d": 3, "c": 2}, {"f": 5, "e": 4}], "a": 1}
+    # after running this through util.sort_dict and message packing, this becomes:
+    #
+    #    82 A1 61 01 A1 62 92 82 A1 63 02 A1 64 03 82
+    #    A1 65 04 A1 66 05 A1 67 82 A1 68 06 A1 69 07
+    #
+    #  83           -- fixmap length 3
+    #    A1 61      -- fixstr length 1 = "a"
+    #    01         -- fixint = 1
+    #    A1 62      -- fixstr length 1 = "b"
+    #    92         -- fixarray length 2
+    #      82       -- fixmap length 2
+    #        A1 63  -- fixstr length 1 = "c"
+    #        02     -- fixint = 2
+    #        A1 64  -- fixstr length 1 = "d"
+    #        03     -- fixint = 3
+    #      82       -- fixmap length 2
+    #        A1 65  -- fixstr length 1 = "e"
+    #        04     -- fixint = 4
+    #        A1 66  -- fixstr length 1 = "f"
+    #        05     -- fixint = 5
+    #    A1 67      -- fixstr length 1 = "g"
+    #    82         -- fixmap length 2
+    #      A1 68    -- fixstr length 1 = "h"
+    #      06       -- fixint = 6
+    #      A1 69    -- fixstr length 1 = "i"
+    #      07       -- fixint = 7
+    #
+    # Given the above, it's easy to compute the MD5 hash of this packed data.  e.g.:
+    #
+    #   $ printf "%b%b" "\x83\xa1a\x01\xa1\x62\x92\x82\xa1\x63\x02\xa1\x64" \
+    #   > "\x03\x82\xa1\x65\x04\xa1\x66\x05\xa1\x67\x82\xa1\x68\x06\xa1\x69\x07" \
+    #   > | md5sum -
+    #   dd18dea4349566ff0290e4d6aeb34035  -
+    assert util.hash_dict(a) == "dd18dea4349566ff0290e4d6aeb34035"
+
+
+def test_state(storage_path):
+    """Test PersistentState.
+
+    This is the original PersistentState test.  But now, some of
+    the stuff tested here is also tested separately above.
+    """
+
+    # Create persistent state
+    p = pathlib.Path(storage_path, "state.json")
+    ps = util.PersistentState(p, missing_ok=True)
+
+    # Test the default state
+    assert ps.state == {}
+
+    test_state = {"message": "Hello World!"}
+
+    # Test that an update can be read in Python space
+    with ps.update():
+        ps.state = test_state
+
+    # Check that state is set to be the same
+    assert ps.state == test_state
+    # ... but also that it is a copy not a reference
+    assert ps.state is not test_state
+    assert ps._state is not test_state
+
+    # Test that the update is available on disk
+    with p.open("r") as fh:
+        disk_state = json.load(fh)
+    assert disk_state == ps.state
+
+    with pytest.raises(exceptions.InternalError) as excinfo:
+        with ps.update():
+            # Update with an unserialisable type to make it fail
+            ps.state = lambda x: x
+
+    assert type(excinfo.value.__cause__) is TypeError
+
+    # Test that the state has not changed
+    assert ps.state == test_state
+
+    # Test that the state has not changed on disk either
+    with p.open("r") as fh:
+        disk_state = json.load(fh)
+    assert disk_state == ps.state
